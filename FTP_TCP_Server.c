e1767b7463f1c6dae7a3ee28ffa0497e644bfc9e
Saumya Shah <94970538+0Chuck0@users.noreply.github.com>
2023-08-27 23:37:39 +0530

    Update FTP_TCP_Server.c

--- FTP_TCP_Server.c
+++ FTP_TCP_Server.c
@@ -1,40 +1,57 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
+#include<unistd.h>
 #include <string.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include<stdlib.h>
+#include<arpa/inet.h>
 
 //Defining the buffer size
 #define SIZE 1024
 
 //write_file function which will make the new file and 
 //write content of the received file by the client
-void write_file(int socket_fd)
+
+void write_file(int socket_fd,char* filename )
 {
     int n;
     FILE* fp;
-    char* filename = "received.txt";
-    char* buffer[1024] = {0};
+    char buffer[SIZE];
 
-    fp = fopen(filename,'w');
+    fp = fopen(filename,"w");
     while(1)
     {
         n = recv(socket_fd,buffer,SIZE,0);
-        if(n<=0)    return;
-        fprintf(fp,%s,buffer);
+        if(n<=0)    return ;
+        fprintf(fp,"%s",buffer);
         bzero(buffer,SIZE);
     }
 
-    return;
+    return ;
 }
+
+void send_file(FILE* fp,int socket_fd)
+{
+	int n;
+	char data[SIZE]={0};
+	int flag;
+	//sending the data until the file gets empty
+	while(fgets(data,SIZE,fp) != NULL)
+	{
+		flag = send(socket_fd,data,SIZE,0);
+		if(flag==-1)
+		{
+			perror("Error in sending the file");
+			exit(1);
+		}
+		bzero(data,SIZE);
+	}
+	
+}
+
 int main()
 {
     //First we will declare the host and port number
-    char * ip_ad = "172.0.0.1";
-    int port = 6969;
+    char * ip_ad = "127.0.0.1";
+    int port = 8080;
     int var;
 
     //Now decalring some important variables
@@ -43,7 +60,7 @@ int main()
     socklen_t address_size;
 
     //creating buffer
-    char buffer[SIZE] = {0};
+    char buffer[SIZE];
 
     //Creating the socket
     server_fd = socket(AF_INET, SOCK_STREAM,0);
@@ -65,8 +82,8 @@ int main()
     address_size = sizeof(server_address);
 
     //bindin the socket with IP and Port number
-    var = bind(server_fd,(struct sockaddr *) &server_address,&address_size);
-    if(e<0)
+    var = bind(server_fd,(struct sockaddr *) &server_address,address_size);
+    if(var<0)
     {
         perror("Socket binding failed !");
         exit(1); 
@@ -86,12 +103,29 @@ int main()
 
     //accepting the connection at new_socket
     address_size = sizeof(new_address);
-    new_socket = accept(server_fd,(struct sockadddr *)&new_address,&address_size);
+    new_socket = accept(server_fd,(struct sockaddr *)&new_address,&address_size);
 
     //writing the data in the new file provided by the client
-    write_file(new_socket);
-
+    char* filename = "received.txt";
+    write_file(new_socket,filename);
     printf("FILE creation was successful\n");
-
+/*
+    //So now i want to make a feature where the user can ask the server to resend the same file
+    //reading the string of the user
+    recv(new_socket,buffer,SIZE,0);
+    if(buffer == "yes")
+    {
+    	FILE *fp = fopen(filename,"r");
+    	if(fp == NULL)
+	{
+		perror("Unable to read the file");
+		exit(1);
+	}
+    	send_file(fp,new_socket);
+    	printf("********\nFIle was sent successfully\n*********\n");
+    	
+    }
+    //so here i am waiting for users confirmation
+    */
     return 0;
 }
